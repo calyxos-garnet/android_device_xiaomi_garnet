91d126fecb66d13894c70cce1349b42037be99b2
UnmoveD <unmoved2121@gmail.com>
2025-05-09 21:48:48 +0200

    garnet: Welcome CalyxOS
    Change-Id: I4d8975ac03556a9e2f232779a482f9731b40edcf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,7 +74,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     hardware/dolby/configs/vintf/dolby_framework_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/calyx/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/hidl/manifest.xml \
